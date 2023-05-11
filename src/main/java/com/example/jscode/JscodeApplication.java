ae377a43986ddbd25294b5cf2d5d2227444c62c7
TunJaek <tasan1605@naver.com>
2023-05-11 11:41:01 +0900

    initial commit

---
+++ src/main/java/com/example/jscode/JscodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.jscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JscodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JscodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jscode/JscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JscodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
